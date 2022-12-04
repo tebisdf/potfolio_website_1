d8cad98c34d7a218484346423a743651755b35e9
tebit237 <64896943+tebit237@users.noreply.github.com>
2022-12-04 11:00:37 +0100

    creating of environment

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <Header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Tebit Roger</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+      </div>
+    </Header>
+  );
+}
